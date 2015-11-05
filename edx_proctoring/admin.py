c4da6a804afd06e0e0494626640fc1c2ce03703a
TamoshaytisV <amd.vladas@gmail.com>
2015-11-05 03:17:50 +0200

    Update admin.py

--- edx_proctoring/admin.py
+++ edx_proctoring/admin.py
@@ -12,8 +12,10 @@ from edx_proctoring.models import (
     ProctoredExamSoftwareSecureReviewHistory,
 )
 from edx_proctoring.utils import locate_attempt_by_attempt_code
-from edx_proctoring.backends import get_backend_provider, get_provider_name_by_course_id
+from edx_proctoring.backends import get_backend_provider
 
+from xmodule.modulestore.django import modulestore
+from opaque_keys.edx.keys import CourseKey
 
 class ProctoredExamReviewPolicyAdmin(admin.ModelAdmin):
     """
@@ -170,7 +172,10 @@ class ProctoredExamSoftwareSecureReviewAdmin(admin.ModelAdmin):
         review.save()
         # call the review saved and since it's coming from
         # the Django admin will we accept failures
-        provider_name = get_provider_name_by_course_id(review.exam['course_id'])
+        course_id = review.exam.course_id
+        course_key = CourseKey.from_string(course_id)
+        course = modulestore().get_course(course_key)
+        provider_name = course.proctoring_service
         get_backend_provider(provider_name).on_review_saved(review, allow_status_update_on_fail=True)
 
     def get_form(self, request, obj=None, **kwargs):
