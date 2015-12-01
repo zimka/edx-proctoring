3ea23759d85d5844ba9891aeab95ef0ae624c85f
TamoshaytisV <amd.vladas@gmail.com>
2015-12-01 18:25:51 +0200

    Update urls.py

--- edx_proctoring/urls.py
+++ edx_proctoring/urls.py
@@ -36,7 +36,7 @@ urlpatterns = patterns(  # pylint: disable=invalid-name
         name='edx_proctoring.proctored_exam.attempt'
     ),
     url(
-        r'edx_proctoring/v1/proctored_exam/attempt/{}$'.format(settings.COURSE_ID_PATTERN),
+        r'edx_proctoring/v1/proctored_exam/attempt/(?P<attempt_code>[-\w]+)$',
         views.StudentProctoredExamAttemptByCode.as_view(),
         name='edx_proctoring.proctored_exam.attempt'
     ),
